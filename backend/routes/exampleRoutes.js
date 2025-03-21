179febfad30f0cbdbcc66b4d68fe57cc1ffb2938
jhon stiven colorado <stivenhenaoronaldo1187@gmail.com>
2025-03-21 13:39:49 -0500

    empezando nuevamente el backend

--- backend/routes/exampleRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const exampleController = require('../controllers/exampleController');
-
-// Ruta de ejemplo
-router.get('/example', exampleController.getExample);
-
-module.exports = router;
